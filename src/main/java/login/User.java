d1b4c1ed8a79809282e0f352593408a079b7d238
franceschini <andrefranceschini@outlook.com.br>
2024-05-08 20:35:00 -0300

    /documentation

--- src/main/java/login/User.java
+++ src/main/java/login/User.java
@@ -1,41 +1,39 @@
-package login;
-
-import java.sql.*;
-
 public class User {
+    // Nodo 1
     public Connection conectarBD() {
-        Connection conn = null;
+        Connection conn = null;  // Nodo 2
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-            conn = DriverManager.getConnection(url);
+            Class.forName("com.mysql.cj.jdbc.Driver");  // Nodo 3
+            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";  // Nodo 4
+            conn = DriverManager.getConnection(url);  // Nodo 5
         } catch (Exception e) {
-            e.printStackTrace(); // Agora imprimindo a exceção para diagnóstico
+            e.printStackTrace();  // Nodo 6
         }
-        return conn;
+        return conn;  // Nodo 7
     }
 
+    // Nodo 8
     public String nome = "";
+    // Nodo 9
     public boolean result = false;
 
+    // Nodo 10
     public boolean verificarUsuario(String login, String senha) {
-        String sql = "";
-        Connection conn = conectarBD();
-        // INSTRUÇÃO SQL
-        sql += "select nome from usuarios ";
-        sql += "where login = ? and senha = ?";
+        String sql = "";  // Nodo 11
+        Connection conn = conectarBD();  // Nodo 12
+        sql += "select nome from usuarios where login = ? and senha = ?";  // Nodo 13
         try {
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, login);
-            ps.setString(2, senha);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                result = true;
-                nome = rs.getString("nome");
+            PreparedStatement ps = conn.prepareStatement(sql);  // Nodo 14
+            ps.setString(1, login);  // Nodo 15
+            ps.setString(2, senha);  // Nodo 16
+            ResultSet rs = ps.executeQuery();  // Nodo 17
+            if (rs.next()) {  // Nodo 18
+                result = true;  // Nodo 19
+                nome = rs.getString("nome");  // Nodo 20
             }
         } catch (Exception e) {
-            e.printStackTrace(); // Agora imprimindo a exceção para diagnóstico
+            e.printStackTrace();  // Nodo 21
         }
-        return result;
+        return result;  // Nodo 22
     }
 }
